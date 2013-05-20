01c8724f91912ff2feccb56b59dc97728066cebd
mredar <mredar@gmail.com>
2013-05-20 11:20:41 -0700

    Remove first attempt at api with python-tastypie-client.

--- collection_registry_client.py
+++ collection_registry_client.py
@@ -1,6 +1,5 @@
 import json
 
-import requests
 import sys, os
 sys.path.insert(0,  os.path.abspath('./python-tastypie-client'))
 
@@ -13,42 +12,6 @@ url_api = url_root+path_collection_registry
 entrypoint_entrypoint_key = "list_entrypoint"
 entrypoint_schema_key = "schema"
 collection_name = "provenancialcollection"
-
-####lass Tastypieentrypoint(object):
-####   '''Tastypie list_entrypoint object'''
-####   pass
-####lass TastypieClient(object):
-####   '''Tastypie (http://tastypieapi.org/) HATEOAS API client'''
-####   def __init__(self, url_api):
-####       self._url_api = url_api
-####       self._entrypoints = None
-
-####   @property
-####   def entrypoints(self):
-####       '''Returns a dict of entrypoint entry points'''
-####       if not self._entrypoints:
-####           self._entrypoints = json.loads(requests.get(url_root+path_collection_registry).text)
-####       return self._entrypoints
-
-####   def find(self, name_id):
-####       '''find the resource entry point for the given resource name?'''
-####       return self.entrypoints.get(name_id, None)
-
-####p = TastypieClient(url_api)
-####c = tp.find(collection_name) #need to return object for entrypoint, study tastypie_client code
-####print pc
-#####'''
-#####url_collection_entrypoint = url_collection_schema = None
-#####for c, d in hateoas.items():
-#####    if c == collection_name:
-#####        url_collection_entrypoint = d[entrypoint_entrypoint_key]
-#####        url_collection_schema = d[entrypoint_schema_key]
-#####entrypoint = json.loads(requests.get(url_root+url_collection_entrypoint).text)
-#####print entrypoint['meta']
-######TODO: wrap entrypoint in iterable object, uses meta & next to keep getting more
-###### when currently downloaded exhausted. Perfect use of generator.
-#####'''
-
 
 tp = tastypie_client.Api(url_api)
 provenancialcollection = None
