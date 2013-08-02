2a8e7e3339f1454c7c883e444c1fe9a70b4a5060
Mark Redar <mredar@gmail.com>
2013-08-02 10:51:29 -0700

    Add repository information to message.

--- collection_registry_client.py
+++ collection_registry_client.py
@@ -10,25 +10,25 @@ url_root = "http://vorol-dev.cdlib.org/"
 url_root = "http://127.0.0.1:8000/"
 path_collection_registry = "api/v1"
 url_api = url_root+path_collection_registry
-collection_name = "provenancialcollection"
+collection_obj_name = "collection"
 
 tp = tastypie_client.Api(url_api)
-provenancialcollection = None
+collection = None
 for c in tp.collections:
     try:
-        c.url.index(collection_name) #this throws if name not found
-        provenancialcollection = c
+        c.url.index(collection_obj_name) #this throws if name not found
+        collection = c
     except:
         pass
 
-print provenancialcollection.url 
+print collection.url 
 
 #queue for OAI
 conn=boto.sqs.connect_to_region('us-east-1')
 q_oai = conn.get_queue(QUEUE_OAI_HARVEST)
 
 obj_list = []
-for obj in provenancialcollection:
+for obj in collection:
     #Currently we only know how to harvest OAI type collections
     #right now, we key off of which fields are found in the object
     #add a message to appropriate queue
@@ -37,6 +37,7 @@ for obj in provenancialcollection:
         msg_dict = { 'url': obj.fields['url_oai'],
                 'set_spec': obj.fields['oai_set_spec'],
                 'campus': obj.fields['campus'],
+                'repository': obj.fields['repository'],
                 'collection_name': obj.fields['name']
                 }
         msg = json.dumps(msg_dict)
